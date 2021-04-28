2ffc23007ea6f2759b88784cb55ecfa919bd9c41
Pierre G <exhaustplume@gmail.com>
2021-04-28 19:06:34 +0100

    Fixed dt to address oscillatory temperature behavior.
    Changed the delta t parameter to keep the (k*dt)/(rho*cp) close to 1e-6. This avoids large oscillations due to changing the material properties but leaving dt fixed at the original 0.5 seconds. Modification of the dt parameter is thus necessary to accomodate changes in material properties.

--- simple conduction/simpleconduction.py
+++ simple conduction/simpleconduction.py
@@ -15,13 +15,13 @@ L = 1                               # Length of element, in meters.
 # Discretization #
 ##################
 
-dx = 0.01                           # Size of discretization step, in meters.
+dx = 0.05                           # Size of discretization step, in meters.
 nx = int(L/dx) + 1                  # Total number of points in the x-direction, unitless. Add one point as there are
                                     # (n+1) points for n elements. Use int to avoid Python breaking.
 x = np.linspace(0,L,nx)             # Creation of the x vector for plotting purposes. It is composed of linearly spaced points.
 
-t_sim = 150                         # Total physical time to simulate, in seconds.
-dt = 0.5                            # Time discretization, in seconds.
+t_sim = 180                         # Total physical time to simulate, in seconds.
+dt = 0.01                            # Time discretization, in seconds.
 nt = int(t_sim/dt)                  # Total number of timesteps, unitless. Use int to avoid Python breaking.
 
 #######################
@@ -30,7 +30,7 @@ nt = int(t_sim/dt)                  # Total number of timesteps, unitless. Use i
 
 rho = 8940                          # Material density, in kilograms per meter cubed. Use 8050 for steel, 8940 for copper.
 cp = 385                            # Material specific heat capacity, in joules per kilogram kelvin. Use 500 for steel, 385 for copper.
-k = 401                              # Material thermal conductivity, in watts per meter kelvin. Use 14.4 for steel, 401 for copper.
+k = 385                             # Material thermal conductivity, in watts per meter kelvin. Use 14.4 for steel, 385 for copper.
 
 ##############################################
 # Initial Conditions and Array Initalization #
@@ -49,18 +49,22 @@ for counter in range(1,nt):         # Use a counter variable to iterate through 
 
     T[0] = 293.15                   # Enforce constant temperature boundary condition at x = 0.
     T[-1] = T[-2]                   # Enforce zero gradient boundary condition at x = L.
+    
 
-    ####################
-    # Plotting Figures #
-    ####################
+    if (counter%100 == 0):
+        ####################
+        # Plotting Figures #
+        ####################
 
-    fig = pyplot.figure(figsize=(11, 7), dpi=100)                               # Create a new figure.
-    ax = pyplot.gca                                                             # Get figure axes.
-    pyplot.plot(x,T)                                                            # Plot the temperature array at the current time step.
-    fig.suptitle('Temperature distribution at t = '+ str(counter) + ' s.')      # Apply titles, labels, then save the figure.
-    pyplot.xlabel('Distance [m]')                                               
-    pyplot.ylabel('Temperature [K]')
-    pyplot.savefig('figures/figure'+str(counter)+'.png', dpi=150, transparent=False, facecolor='#ffffff', bbox_inches='tight', pad_inches=0.25)
+        fig = pyplot.figure(figsize=(11, 7), dpi=100)                               # Create a new figure.
+        ax = pyplot.gca                                                             # Get figure axes.
+        pyplot.plot(x,T)                                                            # Plot the temperature array at the current time step.
+        fig.suptitle('Temperature distribution at t = '+ str(int(counter/(1/dt))) + ' s.')      # Apply titles, labels, then save the figure.
+        pyplot.xlabel('Distance [m]')
+        pyplot.xlim([0, L])                                          
+        pyplot.ylabel('Temperature [K]')
+        pyplot.ylim([T[0], T_A])
+        pyplot.savefig('figures/figure'+str(int(counter/(1/dt)))+'.png', dpi=150, transparent=False, facecolor='#ffffff', bbox_inches='tight', pad_inches=0.25)
 
     ####################
     # Progress Counter #
