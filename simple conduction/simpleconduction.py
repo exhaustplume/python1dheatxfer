5252e8fdd821ffe12b1865e749407eeba416c435
Pierre G <exhaustplume@gmail.com>
2021-04-29 10:24:34 +0100

    Update
    Fixed progress counter.
    Reduced time delta to avoid discretization error.
    Added code to output a single image instead of all images at each time step.

--- simple conduction/simpleconduction.py
+++ simple conduction/simpleconduction.py
@@ -20,8 +20,8 @@ nx = int(L/dx) + 1                  # Total number of points in the x-direction,
                                     # (n+1) points for n elements. Use int to avoid Python breaking.
 x = np.linspace(0,L,nx)             # Creation of the x vector for plotting purposes. It is composed of linearly spaced points.
 
-t_sim = 301                         # Total physical time to simulate, in seconds.
-dt = 0.01                            # Time discretization, in seconds.
+t_sim = 600                         # Total physical time to simulate, in seconds.
+dt = 0.001                          # Time discretization, in seconds.
 nt = int(t_sim/dt)                  # Total number of timesteps, unitless. Use int to avoid Python breaking.
 
 #######################
@@ -51,29 +51,38 @@ for counter in range(1,nt):         # Use a counter variable to iterate through 
     T[-1] = T[-2]                   # Enforce zero gradient boundary condition at x = L.
     
 
-    if (counter%100 == 0):
+    # if (counter%100 == 0):
         ####################
         # Plotting Figures #
         ####################
 
-        fig = pyplot.figure(figsize=(11, 7), dpi=100)                               # Create a new figure.
-        ax = pyplot.gca                                                             # Get figure axes.
-        pyplot.plot(x,T)                                                            # Plot the temperature array at the current time step.
-        fig.suptitle('Temperature distribution at t = '+ str(int(counter/(1/dt))) + ' s.')      # Apply titles, labels, then save the figure.
-        pyplot.xlabel('Distance [m]')
-        pyplot.xlim([0, L])                                          
-        pyplot.ylabel('Temperature [K]')
-        pyplot.ylim([(T[0]-1.15), (T_A+0.85)])
-        pyplot.grid(linestyle = '--', linewidth = 0.5)
-        pyplot.savefig('figures/figure'+str(int(counter/(1/dt)))+'.png', dpi=150, transparent=False, facecolor='#ffffff', bbox_inches='tight', pad_inches=0.25)
+        # fig = pyplot.figure(figsize=(11, 7), dpi=100)                               # Create a new figure.
+        # ax = pyplot.gca                                                             # Get figure axes.
+        # pyplot.plot(x,T)                                                            # Plot the temperature array at the current time step.
+        # fig.suptitle('Temperature distribution at t = '+ str(int(counter/(1/dt))) + ' s.')      # Apply titles, labels, then save the figure.
+        # pyplot.xlabel('Distance [m]')
+        # pyplot.xlim([0, L])                                          
+        # pyplot.ylabel('Temperature [K]')
+        # pyplot.ylim([(T[0]-1.15), (T_A+0.85)])
+        # pyplot.grid(linestyle = '--', linewidth = 0.5)
+        # pyplot.savefig('figures/figure'+str(int(counter/(1/dt)))+'.png', dpi=150, transparent=False, facecolor='#ffffff', bbox_inches='tight', pad_inches=0.25)
 
     ####################
     # Progress Counter #
     ####################
 
-    if (round((counter/nt)*100,2)%5 == 0) :                             # If the step number represents a percentage that is a multiple of 5%, enter the if statement.
+    if (round((counter / nt) * 100,4) % 10 == 0) :                             # If the step number represents a percentage that is a multiple of 5%, enter the if statement.
             print('Progress: ' + str(round((counter/nt)*100,2)) + '%')  # Print the current percentage of time step progress (represented by the counter).
 
+fig = pyplot.figure(figsize=(11, 7), dpi=100)                               # Create a new figure.
+ax = pyplot.gca                                                             # Get figure axes.
+pyplot.plot(x,T)                                                            # Plot the temperature array at the current time step.
+fig.suptitle('Temperature distribution at t = '+ str(int(counter/(1/dt))) + ' s.')      # Apply titles, labels, then save the figure.
+pyplot.xlabel('Distance [m]')
+pyplot.xlim([0, L])                                          
+pyplot.ylabel('Temperature [K]')
+pyplot.ylim([(T[0]-1.15), (T_A+0.85)])
+pyplot.grid(linestyle = '--', linewidth = 0.5)
 time2 = time.perf_counter()                                                                         # Stop the timer for the program execution.
 print('Total execution time for '+str(nt)+' time steps: '+str(round((time2-time1),3))+' seconds.')  # Print the runtime for the program execution.
 # %%
