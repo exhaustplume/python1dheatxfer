d311b628362c4952fe915c7ed0e6cb577aee7662
Pierre G <exhaustplume@gmail.com>
2021-05-10 22:57:00 +0100

    Fixed program commenting.

--- compositematerialconduction/bimaterialcompositeconduction.py
+++ compositematerialconduction/bimaterialcompositeconduction.py
@@ -8,21 +8,20 @@ time1 = time.perf_counter()         # Start the timer for the program execution.
 #######################
 # Geometry Definition #
 #######################
-
-L1 = 0.5                            # Length of element, in meters.
-L2 = 0.5
+L1 = 0.5                            # Length of Material 1, in meters.
+L2 = 0.5                            # Length of Material 2, in meters.
 
 ##################
 # Discretization #
 ##################
 
-dx = 0.0005                           # Size of discretization step, in meters.
+dx  = 0.0005                        # Size of discretization step, in meters.
 nx1 = int(L1 / dx) + 1              # Total number of points in the x-direction, unitless. Add one point as there are
 nx2 = int(L2 / dx) + 1              # Total number of points in the x-direction, unitless. Add one point as there are
-x1 = np.linspace(0,L1,nx1)          # Creation of the x vector for plotting purposes. It is composed of linearly spaced points.
-x2 = np.linspace(0,L2,nx1) + L1          # Creation of the x vector for plotting purposes. It is composed of linearly spaced points.
+x1  = np.linspace(0,L1,nx1)         # Creation of the x vector for plotting purposes. It is composed of linearly spaced points.
+x2  = np.linspace(0,L2,nx1) + L1    # Creation of the x vector for plotting purposes. It is composed of linearly spaced points.
 
-t_sim = 301                        # Total physical time to simulate, in seconds.
+t_sim = 301                         # Total physical time to simulate, in seconds.
 dt = 0.001                          # Time discretization, in seconds.
 nt = int(t_sim/dt)                  # Total number of timesteps, unitless. Use int to avoid Python breaking.
 
@@ -32,7 +31,7 @@ nt = int(t_sim/dt)                  # Total number of timesteps, unitless. Use i
 
 rho1 = 8050                         # Material density, in kilograms per meter cubed. Use 8050 for steel, 8940 for copper.
 cp1 = 500                           # Material specific heat capacity, in joules per kilogram kelvin. Use 500 for steel, 385 for copper.
-k1 = 14.4                            # Material thermal conductivity, in watts per meter kelvin. Use 14.4 for steel, 385 for copper.
+k1 = 14.4                           # Material thermal conductivity, in watts per meter kelvin. Use 14.4 for steel, 385 for copper.
 
 rho2 = 8940                         # Material density, in kilograms per meter cubed. Use 8050 for steel, 8940 for copper.
 cp2 = 385                           # Material specific heat capacity, in joules per kilogram kelvin. Use 500 for steel, 385 for copper.
@@ -43,35 +42,35 @@ k2 = 385                            # Material thermal conductivity, in watts pe
 ##############################################
 
 T_A = 303.15                        # Set the initial condition temperature to 303.15 Kelvin.
-T1 = np.ones(nx1) * T_A             # Initalize the temperature array, and apply the initial condition.
-T2 = np.ones(nx2) * T_A
+T1  = np.ones(nx1) * T_A            # Initalize the temperature array for Material 1, and apply the initial condition.
+T2  = np.ones(nx2) * T_A            # Initalize the temperature array for Material 2, and apply the initial condition.
 
 ###########################
 # Simulation and Plotting #
 ###########################
 
 for counter in range(1,nt):         # Use a counter variable to iterate through each time step.
-    Tn1 = T1.copy()                   # Copy the temperature array from the previous time step to the current time step.
-    Tn2 = T2.copy()                   # Copy the temperature array from the previous time step to the current time step.
+    Tn1 = T1.copy()                 # Copy the temperature array from the previous time step to the current time step for array T1.
+    Tn2 = T2.copy()                 # Copy the temperature array from the previous time step to the current time step for array T2.
 
-    T1[1:-1] = Tn1[1:-1] + ((k1 * dt)/(rho1 * cp1))*((Tn1[2:] - 2 * Tn1[1:- 1] + Tn1[0:-2])/(dx ** 2)) 
-    T2[1:-1] = Tn2[1:-1] + ((k2 * dt)/(rho2 * cp2))*((Tn2[2:] - 2 * Tn2[1:- 1] + Tn2[0:-2])/(dx ** 2)) 
+    T1[1:-1] = Tn1[1:-1] + ((k1 * dt)/(rho1 * cp1))*((Tn1[2:] - 2 * Tn1[1:- 1] + Tn1[0:-2])/(dx ** 2))  # Apply the finite-difference method to calculate the next time step for array T1.
+    T2[1:-1] = Tn2[1:-1] + ((k2 * dt)/(rho2 * cp2))*((Tn2[2:] - 2 * Tn2[1:- 1] + Tn2[0:-2])/(dx ** 2))  # Apply the finite-difference method to calculate the next time step for array T2.
 
     T1[0] = 273                     # Enforce constant temperature boundary condition at x = 0.
     T2[-1] = 323                    # Enforce constant temperature boundary condition at x = L1 + L2.
     
-    T1[-1] = ((k1 * L1 * T1[-2]) + (k2 * L2 * T2[1])) / ((k2 * L1) + (k1 * L2))
-    T2[0] = ((k1 * L1 * T1[-2]) + (k2 * L2 * T2[1])) / ((k2 * L1) + (k1 * L2))                 # Enforce boundary condition at interface.    
+    T1[-1] = ((k1 * L1 * T1[-2]) + (k2 * L2 * T2[1])) / ((k2 * L1) + (k1 * L2))                         # Enforce boundary condition at interface.
+    T2[0]  = ((k1 * L1 * T1[-2]) + (k2 * L2 * T2[1])) / ((k2 * L1) + (k1 * L2))                         # Enforce boundary condition at interface.    
 
     if (counter%100 == 0):
         ####################
         # Plotting Figures #
         ####################
 
-        fig = pyplot.figure(figsize=(11, 7), dpi=100)                               # Create a new figure.
-        ax = pyplot.gca                                                             # Get figure axes.
-        pyplot.plot(x1,T1,x2,T2)                                                            # Plot the temperature array at the current time step.
-        fig.suptitle('Temperature distribution at t = '+ str(int(counter/(1/dt))) + ' s.')      # Apply titles, labels, then save the figure.
+        fig = pyplot.figure(figsize=(11, 7), dpi=100)                                                   # Create a new figure.
+        ax = pyplot.gca                                                                                 # Get figure axes.
+        pyplot.plot(x1,T1,x2,T2)                                                                        # Plot both temperature arrays at the current time step.
+        fig.suptitle('Temperature distribution at t = '+ str(int(counter/(1/dt))) + ' s.')              # Apply titles, labels, then save the figure.
         pyplot.xlabel('Distance [m]')
         pyplot.xlim([0, (L1 + L2)])                                          
         pyplot.ylabel('Temperature [K]')
@@ -83,18 +82,9 @@ for counter in range(1,nt):         # Use a counter variable to iterate through 
     # Progress Counter #
     ####################
 
-    if (round((counter / nt) * 100,8) % 10 == 0) :                             # If the step number represents a percentage that is a multiple of 5%, enter the if statement.
-            print('Progress: ' + str(round((counter/nt)*100,2)) + '%')  # Print the current percentage of time step progress (represented by the counter).
+    if (round((counter / nt) * 100,8) % 10 == 0) :                                                      # If the step number represents a percentage that is a multiple of 5%, enter the if statement.
+            print('Progress: ' + str(round((counter/nt)*100,2)) + '%')                                  # Print the current percentage of time step progress (represented by the counter).
 
-# fig = pyplot.figure(figsize=(11, 7), dpi=100)                               # Create a new figure.
-# ax = pyplot.gca                                                             # Get figure axes.
-# pyplot.plot(x1,T1,x2,T2)                                                            # Plot the temperature array at the current time step.
-# fig.suptitle('Temperature distribution at t = '+ str(int(counter/(1/dt))) + ' s.')      # Apply titles, labels, then save the figure.
-# pyplot.xlabel('Distance [m]')
-# pyplot.xlim([0, (L1 + L2)])                                          
-# pyplot.ylabel('Temperature [K]')
-# pyplot.ylim([270, 325])
-# pyplot.grid(linestyle = '--', linewidth = 0.5)
-time2 = time.perf_counter()                                                                         # Stop the timer for the program execution.
-print('Total execution time for '+str(nt)+' time steps: '+str(round((time2-time1),3))+' seconds.')  # Print the runtime for the program execution.
+time2 = time.perf_counter()                                                                             # Stop the timer for the program execution.
+print('Total execution time for '+str(nt)+' time steps: '+str(round((time2-time1),3))+' seconds.')      # Print the runtime for the program execution.
 # %%
